d455c18ab542601a27bdb14840e8024b3ad6bf8c
Yang Hu <gymf1010@email.arizona.edu>
2020-07-26 18:27:32 +0800

    improvements on enemy AI performance

--- Assets/enemyAI_YH/Scripts/enemy_main.cs
+++ Assets/enemyAI_YH/Scripts/enemy_main.cs
@@ -10,7 +10,7 @@ public class enemy_main : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Debug.Log(enemy_move_direction);
+        
     }
 
     // Update is called once per frame
