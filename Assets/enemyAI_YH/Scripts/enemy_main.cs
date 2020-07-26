ba3ed9d8cda5888b12097940beb20e10ce3bc499
Yang Hu <gymf1010@email.arizona.edu>
2020-07-26 17:53:16 +0800

    added path follow mechanic

--- Assets/enemyAI_YH/Scripts/enemy_main.cs
+++ Assets/enemyAI_YH/Scripts/enemy_main.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class enemy_main : MonoBehaviour
 {
-	public float enemy_speed;
-	public enemy_target where_to_go;
-	private Vector3 enemy_move_direction;
+	public  float          enemy_speed;
+	public  enemy_target   where_to_go;
+	private Vector3        enemy_move_direction;
     // Start is called before the first frame update
     void Start()
     {
-        enemy_move_direction = Vector3.Normalize( where_to_go.transform.position - this.transform.position );
         //Debug.Log(enemy_move_direction);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        this.transform.position += enemy_speed * enemy_move_direction * Time.deltaTime;
+        // find the current target dir
+        where_to_go = where_to_go.update_target(this);
+        if (where_to_go) {
+
+            Debug.Log("The target: " + where_to_go.transform.position);
+            Debug.Log("The enemy   " + this.transform.position);
+            enemy_move_direction    =   Vector3.Normalize( where_to_go.transform.position - this.transform.position );
+            Debug.Log(enemy_move_direction);
+            // move toward that direction
+            this.transform.position +=  enemy_speed * enemy_move_direction * Time.deltaTime;
+        }
     }
 }
--- Assets/enemyAI_YH/Scripts/enemy_target.cs
+++ Assets/enemyAI_YH/Scripts/enemy_target.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class enemy_target : MonoBehaviour
 {
+	public  enemy_target 	next_target;
+	public 	float 			arrival_radius;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+    	Debug.Log("1 " + this + ": " + this.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    // ask a node for new target
+    public enemy_target update_target( enemy_main curr_enemy ) {
+    	float distance = Vector3.Distance(curr_enemy.transform.position, this.transform.position);
+    	//Debug.Log(distance);
+    	if(distance > arrival_radius || !next_target)
+    	{
+    		return this;
+    	}
+    	return next_target;
     }
 }
