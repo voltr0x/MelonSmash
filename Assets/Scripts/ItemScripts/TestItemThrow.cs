50a845f075225a71d140c039b48a7808062b1023
Afrazwastaken <afraaz.zs@gmail.com>
2020-07-27 20:12:23 +0530

    I'm finally here, Character scripts

---
+++ Assets/Scripts/ItemScripts/TestItemThrow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestItemThrow : MonoBehaviour
+{
+        int itemsPickedup;
+        GameObject currentItem;
+        int countItemThrown;
+        // Update is called once per frame
+        void Update()
+        {
+            itemsPickedup = GameObject.FindGameObjectsWithTag("Item_pickedup").Length;
+            currentItem = GameObject.FindGameObjectWithTag("Itemthrown");
+            countItemThrown = GameObject.FindGameObjectsWithTag("Itemthrown").Length;
+
+        if (Input.GetMouseButtonDown(1))
+            {
+                Throw();
+            }
+        }
+        void Throw()
+        {
+            if (itemsPickedup >= 1)
+            {
+                {
+                    this.transform.SetParent(null);
+                    var screenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+                    screenPoint.z = 10.0f;
+                    transform.position = Camera.main.ScreenToWorldPoint(screenPoint);
+                    this.tag = "Itemthrown";
+                }
+            }
+        }
+}
---
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float motionspeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputHorizontal = Input.GetAxis("Horizontal");
+        float inputVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = inputHorizontal * motionspeed * Time.deltaTime;
+        float moveVertical = inputVertical * motionspeed * Time.deltaTime;
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+        transform.position += movement;
+    }
+}
