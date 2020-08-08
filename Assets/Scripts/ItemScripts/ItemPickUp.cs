782c438eb72e6b0e3f2a69fefdefa332761c16e1
Afrazwastaken <afraaz.zs@gmail.com>
2020-08-09 00:38:40 +0530

    Fixed Damage and Melon Spawner

---
+++ Assets/Scripts/ItemScripts/ItemPickUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour
+{
+    Transform playerLocation;
+    int maxChildCount = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerLocation = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void OnMouseDown()
+    {
+        if (playerLocation.childCount < maxChildCount && this.tag == "Item")
+        {
+            transform.SetParent(playerLocation);
+            transform.position = playerLocation.position + new Vector3(0.3f, 0.6f, 0);
+            this.tag = "Item_pickedup";
+        }
+    }
+}
--- Assets/Scripts/enemy_main.cs
+++ Assets/Scripts/enemy_main.cs
@@ -8,6 +8,8 @@ public class enemy_main : MonoBehaviour
     public  enemy_target   where_to_go;
     private Vector3        enemy_move_direction;
     public  int            health = 10;
+    [HideInInspector] public GameObject scriptAObject;
+    ItemThrowVersion2 scriptA;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,18 +22,36 @@ public class enemy_main : MonoBehaviour
         // find the current target dir
         where_to_go = where_to_go.update_target(this);
         // if died or reached the end point, self-destruction
-        if(health <= 0 || where_to_go.tag == "objectDestroyer"){
+        if(health <= 0 || where_to_go.tag == "objectDestroyer")
+        {
             Destroy(this.gameObject);
-        } else {
+        }
+        else
+        {
             enemy_move_direction     =   Vector3.Normalize( where_to_go.transform.position - this.transform.position );
             // move toward that direction
             this.transform.position +=  enemy_speed * enemy_move_direction * Time.deltaTime;
         }
     }
 
-    void take_damage(int damage) {
-
+    void take_damage(int damage)
+    {
         health -= damage;
         Debug.Log("Uhhhhh " + health);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Itemthrown")
+        {
+            scriptAObject = collider.gameObject;
+            scriptA = scriptAObject.GetComponent<ItemThrowVersion2>();
+            scriptA.itemExploded2 = true;
+            if (scriptA.itemExploded2 == true && scriptA.itemAliveTime <= 0.5)
+            {
+                take_damage(scriptA.damage);
+                scriptA.itemExploded2 = false;
+            }
+        }
     }
 }
