61735b3fe78451309bf559fe399c667d262274d6
Afrazwastaken <afraaz.zs@gmail.com>
2020-08-09 19:29:55 +0530

    Kick Function

--- Assets/Scripts/PlayerScripts/PlayerKick.cs
+++ Assets/Scripts/PlayerScripts/PlayerKick.cs
@@ -4,27 +4,45 @@ using UnityEngine;
 
 public class PlayerKick : MonoBehaviour
 {
-    Ray rayDirection;
-    Vector3 inputVertical = new Vector3(0, 1, 0), inputHorizontal = new Vector3(1, 0, 0);
-    RaycastHit hit;
+    public float rayLength = 10;
+    Ray raySetDirection, rayCurrentDirection;
+    Vector3 inputVertical, inputHorizontal, kickedDirection;
+    public GameObject scriptAObject;
+    enemy_main scriptA;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        raySetDirection = new Ray(this.transform.position, Vector3.down);
     }
 
     // Update is called once per frame
     void Update()
     {
+        inputVertical = new Vector3(0, 1, 0) * Input.GetAxis("Vertical");
+        inputHorizontal = new Vector3(1, 0, 0) * Input.GetAxis("Horizontal");
+        RaycastHit hit;
         if (Input.GetAxis("Horizontal") != 0)
         {
-            rayDirection = new Ray(this.transform.position, inputHorizontal * Input.GetAxis("Horizontal"));
+            raySetDirection = new Ray(this.transform.position, inputHorizontal);
+            kickedDirection = inputHorizontal;
         }
         else if (Input.GetAxis("Vertical") != 0)
         {
-            rayDirection = new Ray(this.transform.position, inputVertical * Input.GetAxis("Vertical"));
+            raySetDirection = new Ray(this.transform.position, inputVertical);
+            kickedDirection = inputVertical;
         }
+        rayCurrentDirection = raySetDirection;
         
-        Physics.Raycast(rayDirection, 10);
+        if (Physics.Raycast(rayCurrentDirection, out hit, rayLength))
+        {
+            if (hit.collider.tag == "Enemy" && Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("Enemy Kicked");
+                scriptAObject = hit.collider.gameObject;
+                scriptA = scriptAObject.GetComponent<enemy_main>();
+                scriptA.being_kicked(kickedDirection);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/PlayerScripts/PlayerKick2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKick2 : MonoBehaviour
+{
+    public float rayLength = 10;
+    Ray rayCurrentDirection;
+    public GameObject scriptAObject;
+    enemy_main scriptA;
+    [HideInInspector] public Vector3 newLocation, kickedDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("MouseDown");
+            var screenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+            screenPoint.z = 10.0f;
+            newLocation = Camera.main.ScreenToWorldPoint(screenPoint);
+            kickedDirection = Vector3.Normalize(newLocation - this.transform.position);
+            Debug.Log(kickedDirection);
+            Debug.DrawRay(this.transform.position, kickedDirection * rayLength, Color.black);
+            RaycastHit2D hit = Physics2D.Raycast(this.transform.position, kickedDirection, rayLength);
+            if (hit.collider.tag == "Enemy")
+            {
+                Debug.Log("Enemy Kicked");
+                scriptAObject = hit.collider.gameObject;
+                scriptA = scriptAObject.GetComponent<enemy_main>();
+                scriptA.being_kicked(kickedDirection);
+            }
+        }
+    }
+}
